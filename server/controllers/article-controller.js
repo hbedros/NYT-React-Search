2c76e97b2c4191bf568c34f8406f471d386d64f8
haigbedros <haigbedros@gmail.com>
2018-04-20 10:23:34 -0700

    added article model, article controller methods, a components folder, and updated server.js

--- server/controllers/article-controller.js
+++ server/controllers/article-controller.js
@@ -1,34 +1,28 @@
 var Article = require("../models/Article");
 
 module.exports = {
-  // this method handles finding all articles in the db
+  // this method handles finding articles in the db
   find: function(req, res) {
-    console.log("Gathering saved articles from the db");
-    Article.find().then(function(doc) {
+    Article.find(req.query).then(function(doc) {
       res.json(doc);
     }).catch(function(err) {
       res.json(err);
     });
   },
-  // this method handles adding new articles to the db
+  // this method handles creating new articles
   insert: function(req, res) {
-    console.log("Adding saved artice to the db");
-    console.log("req.body: ", req.body);
     Article.create(req.body).then(function(doc) {
       res.json(doc);
-      console.log("doc: ", doc);
     }).catch(function(err) {
       res.json(err);
     });
   },
   // this method handles deleting articles from the db
   delete: function(req, res) {
-    console.log("Deleting a saved article from the db");
     Article.remove({
       _id: req.params.id
     }).then(function(doc) {
       res.json(doc);
-      console.log("doc: ", doc);
     }).catch(function(err) {
       res.json(err);
     });
